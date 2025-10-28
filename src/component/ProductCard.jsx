a4b3345a6e7718ebf61e86f210723aaedb36008f
eunhye <dmsgp2627@naver.com>
2025-10-28 11:30:27 +0900

    타이틀 가격 추가

--- src/component/ProductCard.jsx
+++ src/component/ProductCard.jsx
@@ -2,20 +2,27 @@ const ProductCard = ({ productList }) => {
     return (
         <div>
             <div></div>
-         
+
 
             <div className="flex flex-wrap justify-center items-center gap-8 p-10">
+
                 {productList.map((product, i) => (
-                    <>
+                    <div key={i} className="w-80 text-center">
                         <img
                             key={i}
                             className=" w-80 h-80 "
                             src={product?.img} />
 
-      
-                    </>
+                        <div>
+                            <h3> {product?.title}</h3>
+                            <p>{product?.price} </p>
+                            <p>{product?.new}</p>
+                        </div >
+                    </div>
 
                 ))}
+
+
             </div>
         </div>
     )
